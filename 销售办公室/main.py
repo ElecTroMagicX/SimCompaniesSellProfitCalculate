65ae6c85fb9ef56ebc75ec316c5659330fcd1342
ElecTroMagicX <934112474@qq.com>
2025-04-11 04:31:05 +0800

    fix 库存计算错误

--- 销售办公室/main.py
+++ 销售办公室/main.py
@@ -1,9 +1,12 @@
+from xmlrpc.client import MAXINT
 from api import SimCompaniesAPI
-import config
+from config import KIND_TABLE
+
 
 def calculate_stock_deficit(api, stocks, buildings):
     """计算库存缺口"""
     stock_deficit = {}
+    all_kinds_amount = {}
 
     for building in filter(lambda b: b.get("kind") == "B", buildings):
         building_id = building.get("id")
@@ -12,11 +15,29 @@ def calculate_stock_deficit(api, stocks, buildings):
 
         orders = api.get_sales_orders(building_id)
         for order in filter(lambda o: len(o.get("resources", [])) > 0, orders):
-            print('--订单信息:', order)
+            print("--订单信息:", order)
             for res in order.get("resources", []):
                 kind = res.get("kind")
-                deficit = stocks.get(kind, {}).get("amount", 0) - res.get("amount", 0)
-                stock_deficit[kind] = {"amount": deficit}
+                lower_price = all_kinds_amount.get(kind, {}).get("price", MAXINT)
+                all_kinds_amount[kind] = {
+                    "amount": all_kinds_amount.get(kind, {}).get('amount', 0) + res.get("amount", 0),
+                    "price": (
+                        res.get("price", 0)
+                        if res.get("price") < lower_price
+                        else lower_price
+                    ),
+                }
+
+    print("all_kinds_amount:", all_kinds_amount)
+    for kind, data in all_kinds_amount.items():
+        deficit = data['amount'] - stocks.get(kind, {}).get("amount", 0)
+        if deficit > 0:
+            stock_deficit[kind] = deficit
+            print(f"货物类型: {KIND_TABLE[kind]}, 需要购买: {deficit}, 最高0星价格: {data['price']}")
+        else:
+            print(
+                f"货物类型: {KIND_TABLE[kind]}, 库存充足: {stocks.get(kind, {}).get('amount', 0)}"
+            )
 
     return stock_deficit
 
@@ -44,7 +65,7 @@ def main():
     # 输出需要购买的物品
     buying_str = "Buying\n"
     for kind, data in stock_deficit.items():
-        buying_str += f"{data['amount']} {config.kindTable.get(kind, f'unknown: :re-{kind}: ')}\n"
+        buying_str += f"{data} {KIND_TABLE.get(kind, f'unknown: :re-{kind}: ')}\n"
     print(buying_str)
 
 
